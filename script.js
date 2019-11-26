5df86db2f2696dbdb176e794e1088d2e1316bb91
Alex Dollard <alexdollard141@gmail.com>
2019-11-26 17:46:07 -0600

    further styling changes

--- script.js
+++ script.js
@@ -1,35 +1,31 @@
 
+let city=""; 
+let url="";
+let APIkey="";
+let queryurl ="";
 
 
-let url = "api.openweathermap.org/data/2.5/forecast?q=";    
+$("#searchbtn").on("click", function(event){
+    event.preventDefault();
+    debugger; 
+     city = $(this).prev().val().trim()
+      url = "https://api.openweathermap.org/data/2.5/forecast?q=";    
 // let city = "richmond";
-let APIkey = "&mode=xml&appid=5ce8439fd4264478d1da0b24a7cd547d";
-let queryurl = url + city + APIkey;
-let city = ""; 
+     APIkey = "&mode=xml&appid=5ce8439fd4264478d1da0b24a7cd547d";
+ queryurl = url + city + APIkey;
 
-function getWeather(){
+
     $.ajax({
         url: queryurl,
         method: "GET",
         
     }).then(function(response){
-        city = $("#search_bar").val().trim() 
         console.log(queryurl);
         console.log(response);  
      })
-    }
-
-
-$("#search_bar").on("click", function(event){
-    event.preventDefault();
-    getWeather(); 
+    
 
 })
-
-
-
-
-
 
 
 
