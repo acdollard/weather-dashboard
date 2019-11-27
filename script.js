48b45322cb9d9b72a86666c1b08f70c155e9b6b3
Alex Dollard <alexdollard141@gmail.com>
2019-11-27 14:53:56 -0600

    Got current weather display functioning
    still need to get the kelvin to Fahrenheit conversion correct

--- script.js
+++ script.js
@@ -3,15 +3,18 @@ let city="";
 let url="";
 let APIkey="";
 let queryurl ="";
+let currenturl = "";
 
 
 $("#searchbtn").on("click", function(event){
     event.preventDefault();
      city = $(this).prev().val().trim()
       url = "https://api.openweathermap.org/data/2.5/forecast?q=";    
+      currenturl = "https://api.openweathermap.org/data/2.5/weather?q=";
 // let city = "richmond";
      APIkey = "&appid=5ce8439fd4264478d1da0b24a7cd547d";
  queryurl = url + city + APIkey;
+ current_weather_url = currenturl + city + APIkey; 
 
 
     $.ajax({
@@ -19,8 +22,25 @@ $("#searchbtn").on("click", function(event){
         method: "GET",
         
     }).then(function(response){
-        console.log(queryurl);
-        console.log(response);  
+        console.log(response);
+        console.log(response.list[1].dt_txt);  
+        let time = response.list[1].dt_txt;
+        
+        console.log(time.split(" ")[1]);
+        // let split_time = time.split(" ")[1];
+
+     });
+
+     $.ajax({
+         url:current_weather_url,
+         method: "GET", 
+     }).then(function(current_data){
+         console.log(current_data);
+         console.log("The temperature in " + city + " is: " + current_data.main.temp);
+         $("#today_temp").text("Temperature: " + (current_data.main.temp - 273.15 * (9/5) + 32));
+         $("#today_humidity").text("Humidity: " + current_data.main.humidity);
+         $("#today_wind_speed").text("Wind Speed: " + current_data.wind.speed);
+         $("#today_icon_div").attr("src", "http://openweathermap.org/img/w/" + current_data.weather[0].icon + ".png");
      })
     
 
